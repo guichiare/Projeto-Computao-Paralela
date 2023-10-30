cd49846cc6018260a5397a7e9f2eb9072484f903
Cloud User <ec2-user@ip-172-31-86-13.ec2.internal>
2023-10-30 12:48:17 +0000

    lab5 update

---
+++ lab5/lab5S.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+#include<math.h>
+
+double f(double x);
+void Trap(double a, double b, int n, double* global_result);
+
+int main(int argc, char* argv[])
+{
+	double global_result = 0.0;
+	double a, b;
+	int n;
+
+	printf("Enter a, b and n: ");
+	scanf("%lf %lf %d", &a, &b, &n);
+	printf("Init pos: %.1lf   End pos: %.1lf   Seg num: %d \n", a, b, n);
+	Trap(a, b, n, &global_result);
+
+	printf("Approx result: %.1f \n", global_result);
+	return 0;
+}
+
+double f(double x)
+{
+	return exp(x);
+}
+
+void Trap(double a, double b, int n, double* global_result)
+{
+	double h, x, result;
+	
+	h = (b-a)/n;
+	result = (f(a) + f(b))/2;
+	
+	for(int i = 1; i <= n-1; i++)
+	{
+		x = a + i * h;
+		result += f(x);
+		printf("%lf\n", result);
+	}
+	result = result*h;
+
+	*global_result += result;
+}
